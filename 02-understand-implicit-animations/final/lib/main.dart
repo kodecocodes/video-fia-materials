4832647f0a28398eda1cf7ac1984389c85cbafff
Yogesh Choudhary <youzendachoudhary22@gmail.com>
2022-08-12 10:04:35 +0530

    updated the episode 2 starter & final project

--- 02-understand-implicit-animations/final/lib/main.dart
+++ 02-understand-implicit-animations/final/lib/main.dart
@@ -1,10 +1,49 @@
+// Copyright (c) 2022 Razeware LLC
+
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following
+// conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// Notwithstanding the foregoing, you may not use, copy, modify,
+// merge, publish, distribute, sublicense, create a derivative work,
+// and/or sell copies of the Software in any work that is designed,
+// intended, or marketed for pedagogical or instructional purposes
+// related to programming, coding, application development, or
+// information technology. Permission for such use, copying,
+// modification, merger, publication, distribution, sublicensing,
+// creation of derivative works, or sale is expressly withheld.
+
+// This project and source code may use libraries or frameworks
+// that are released under various Open-Source licenses. Use of
+// those libraries and frameworks are governed by their own
+// individual licenses.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -14,18 +53,18 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter: Implicit Animations'),
+      home: const MyHomePage(title: 'Flutter: Implicit Animations'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -43,9 +82,15 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: AnimatedOpacity(
           opacity: _showBox ? 1.0 : 0.0,
-          duration: Duration(milliseconds: 1000),
+          // Update Note:  As per the latest flutter linting(at the time of
+            // updating). It is suggested that to use const keyword with
+            // constant constructors to improve the app performance
+          duration:  const Duration(milliseconds: 1000),
           child: AnimatedContainer(
-            duration: Duration(milliseconds: 1000),
+            // Update Note:  As per the latest flutter linting(at the time of
+            // updating). It is suggested that to use const keyword with
+            // constant constructors to improve the app performance
+            duration: const Duration(milliseconds: 1000),
             width: _showBox ? 200 : 100,
             height: _showBox ? 200 : 100,
             color: _showBox ? Colors.orange : Colors.green,
@@ -63,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
           });
         },
         tooltip: 'Increment',
-        child: Icon(Icons.play_arrow),
+        child: const Icon(Icons.play_arrow),
       ),
     );
   }
